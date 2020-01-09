fe74e59813fb6fb27e1490fd4a4e65a523ac6ef6
Kyle Hayes <khayes847@gmail.com>
2020-01-09 15:05:48 -0500

    Add sorting functions

--- loudoun_wildlife_conservancy.py
+++ loudoun_wildlife_conservancy.py
@@ -2,7 +2,7 @@
 February 2020, creates pandas dataframe of calendar information.
 'https://loudounwildlife.org/events/'"""
 
-from datetime import datetime, timedelta
+from datetime import datetime as d
 import json
 import logging
 import re
@@ -32,7 +32,8 @@ def event_details(event):
     event_dict['Event End Time'] = event['endDate'][11:19]
     event_dict['All Day Event'] = False
     event_dict['Timezone'] = 'America/New_York'
-    event_dict['Event Venue Name'] = event['location']['name']
+    event_dict['Event Venue Name'] = location(event)
+    event_dict = lat_long(event_dict, event)
     event_dict['Event Organizers'] = 'Loudoun Wildlife Conservancy'
     event_dict['Event Cost'] = fees(event_dict['Event Description'])
     event_dict['Event Currency Symbol'] = '$'
@@ -54,12 +55,18 @@ def event_description(url):
 
 def fees(event):
     if 'Fee:' in event:
+        fee_dict = {}
         fee_list = re.findall(r"[-+]?\d*\.\d+|\d+", event)
-        return fee_list
-    return []
+        fee_dict['Members'] = fee_list[0]
+        fee_dict['Non-members'] = fee_list[1]
+        if 'free for members' in event.lower():
+            fee_dict['Members'] = '0'
+            return fee_dict
+        return fee_dict
+    return str('None')
 
-def main():
-    soup = bs4_page('https://loudounwildlife.org/events/')
+def month(url):
+    soup = bs4_page(url)
     scripts = soup.find_all('script', {'type':'application/ld+json'})[1:]
     events = []
     for tag in scripts:
@@ -67,4 +74,37 @@ def main():
         for event in event_list:
             event_dict = event_details(event)
             events.append(event_dict)
+    return events
+
+def location(event):
+    if 'location' in event:
+        if 'geo' in event['location']:
+            return str(event['location']['name'])
+    return str('None listed')
+
+def lat_long(event_dict, event):
+    if 'location' in event:
+        
+        return str(event['location']['geo']['latitude'])
+    return str('None listed')
+
+def lat_long(event_dict, event):
+    if 'location' in event:
+        if 'geo' in event['location']:
+            event_dict['latitude'] = str(event['location']['geo']['latitude'])
+            event_dict['longitude'] = str(event['location']['geo']['longitude'])
+            return event_dict
+    event_dict['latitude'] = 'None listed'
+    event_dict['longitude'] = 'None listed'
+    return event_dict
+
+def clean(events):
+    events.sort(key = lambda x: x['Event Start Date'])
+    new_events = [i for n, i in enumerate(events) if i not in events[(n+1):]]
+    return new_events
+
+def main():
+    events = month('https://loudounwildlife.org/events/')
+    events += month('https://loudounwildlife.org/events/2020-02/')
+    events = clean(events)
     return events
